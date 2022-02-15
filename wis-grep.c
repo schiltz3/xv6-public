178c15c2e247c8ebc1744a98ff9094201626a82c
John Schiltz <johnschiltz2000@gmail.com>
2022-02-14 22:08:39 -0600

    work out types

--- wis-grep.c
+++ wis-grep.c
@@ -4,9 +4,42 @@
 #include "user.h"
 #include "fcntl.h"
 
-#define NUM_FILES 3
+#define NULL 0
+
+typedef enum {
+  NOT_FOUND
+
+}fileError;
+
+typedef struct fileOption{
+  fileError error;
+  char *error_str;
+  int fd;
+}fileOption;
+
+void getFile(fileOption* file, char *fileName, int len);
 
 int main(int argc, char *argv[])
 {
+
+  fileOption file = {NOT_FOUND, NULL, 0};
+  switch (argc){
+  case 0:
+    printf(1, "wis-grep: searchterm [file ...]\n");
+    return 1;
+    break;
+  case 1:
+    getFile(&file, argv[1], strlen(argv[1]));
+    break;
+    
+
+  default:
+    printf(1, "default");
+
+  }
+
   return 0;
+}
+
+void getFile(fileOption* file, char *fileName, int len){
 }
